421d2086d3329ca0e9ecb225ead4bea96c0c97e6
XINGchi2003 <97927256+XINGchi0130@users.noreply.github.com>
2022-08-10 19:29:21 +0800

    增加了student的“专业”属性 加了jquery

--- CTGU/recruit/src/main/Person/Student.java
+++ CTGU/recruit/src/main/Person/Student.java
@@ -1,28 +1,31 @@
 package main.Person;
 
-public class Students {
+public class Student {
     private int ID;
     private String Name;
     private String Sex;
     private String IntentionFirst;
     private String IntentionSecond;
     private String College;
+    private String Major;
     private String PhoneNumber;
     private String QQNumber;
     private String Introduction;
 
 
-    public Students(int ID, String name, String sex, String intentionFirst, String intentionSecond, String college, String phoneNumber, String QQNumber, String introduction) {
+    public Student(int ID, String name, String sex, String intentionFirst, String intentionSecond, String college, String major, String phoneNumber, String QQNumber, String introduction) {
         this.ID = ID;
         Name = name;
         Sex = sex;
         IntentionFirst = intentionFirst;
         IntentionSecond = intentionSecond;
         College = college;
+        Major = major;
         PhoneNumber = phoneNumber;
         this.QQNumber = QQNumber;
         Introduction = introduction;
     }
+
 
     public int getID() {
         return ID;
@@ -46,6 +49,10 @@ public class Students {
 
     public String getCollege() {
         return College;
+    }
+
+    public String getMajor() {
+        return Major;
     }
 
     public String getPhoneNumber() {
